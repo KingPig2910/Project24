346bc0592f26202e33a8c7e422f7915fb8fc3747
KingPig2910 <73672641+KingPig2910@users.noreply.github.com>
2020-11-29 18:29:43 -0500

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,25 +1,28 @@
-class Ground {
-    constructor(x,y,width,height){
+class Paper {
+    constructor(x, y, r){
         var options = {
-            isStatic:true
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
         }
         this.x=x;
         this.y=y;
-        this.width=width;
-        this.height=height;
-        this.body = Bodies.rectangle(x,y,width,height,options);
-        World.add(world,this.body);
-
+        this.r=r;
+        this.body = Bodies.circle(this.x, this.y, this.r/2, options);
+     World.add(world, this.body);
     }
-    display(){
-        var groundPos = this.body.position;
-    
-        push()
-        translate(groundPos.x, groundPos.y);
-        rectMode(CENTER);
-        strokeWeight(3);
-        fill("white");
-        rect(0,0,this.width, this.height);
-        pop()
-        }
+  
+
+display(){
+    var paperPos = this.body.position;
+
+    push()
+    translate(paperPos.x, paperPos.y);
+    rectMode(CENTER);
+    strokeWeight(3);
+    fill("white");
+    ellipse(0,0,this.r, this.r);
+    pop()
+    }
 }
